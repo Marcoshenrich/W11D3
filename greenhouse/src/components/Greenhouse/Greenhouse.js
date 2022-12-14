e37ed68b93002883036eadc4f0e070b115c87a98
Marcos <jmarcoshenrich@gmail.com>
2022-12-14 15:00:23 -0800

    day/night cycle complete

--- greenhouse/src/components/Greenhouse/Greenhouse.js
+++ greenhouse/src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,33 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext.js'
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
 
+  const { themeName, setThemeName } = useTheme()
+
+
+
+
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+
+      {( themeName === "day") && <img
+        className='greenhouse-img'
+        src={dayImage}
+        alt='greenhouse'
+      />}
+
+      {( themeName === "night") && <img
+        className='greenhouse-img'
+        src={nightImage}
+        alt='greenhouse'
+      />}
+    
       <LightSwitch />
       <ClimateStats />
     </section>
