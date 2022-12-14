840d02266d7335a0a2d4379dbc8dc2b36059710f
Marcos <jmarcoshenrich@gmail.com>
2022-12-14 12:20:29 -0800

    part 1 phase 1

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,14 @@
+import CoffeeProvider, { useCoffeeContext } from '../context/CoffeeContext';
+
+
 const SelectedCoffeeBean = () => {
-  return null;
+  const { coffeeBean, setCoffeeBean } = useCoffeeContext()
+  console.log(coffeeBean);
+  return (
+    <div className="selected-coffee">
+      <h2>{coffeeBean.name}</h2>
+    </div>
+  );
 }
 
 export default SelectedCoffeeBean;
