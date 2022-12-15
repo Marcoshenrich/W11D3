e15d01ed85558b7a6a0cb049ad7093ba171c1bba
XiWang <vivian.xi.wang@gmail.com>
2022-12-14 16:15:24 -0800

    Finisehed Phase 4

--- greenhouse/src/components/Thermometer/Thermometer.js
+++ greenhouse/src/components/Thermometer/Thermometer.js
@@ -1,14 +1,22 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
+import ClimateProvider, { useClimate } from '../../context/ClimateContext.js'
+
 
 function Thermometer() {
+  const {temperature, setTemperature} = useClimate()
+  console.log(temperature);
+  const slideHandler =(e) =>{
+    // e.preventDefault();
+    setTemperature(e.target)
+  }
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {"x"}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={temperature}
+        onAfterChange={slideHandler}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
